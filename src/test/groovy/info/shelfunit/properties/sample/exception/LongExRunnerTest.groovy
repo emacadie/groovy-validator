abb0b0e00b0ce90e1faed3c00b9c09b79ff09460
Eric MacAdie <Eric@MacAdie.net>
2014-08-17 11:44:19 -0500

    Rearranging and improving tests

--- src/test/groovy/info/shelfunit/properties/sample/exception/LongExRunnerTest.groovy
+++ src/test/groovy/info/shelfunit/properties/sample/exception/LongExRunnerTest.groovy
@@ -33,12 +33,26 @@ class LongExRunnerTest extends Specification {
         lr.thirdNum  == 50L
         
         when:
+        lr.thirdNum  = 5L
+        lr.firstNum  = 1001L
+        lr.secondNum = 1001L
+        then:
+        final GroovyValidatorException exception2 = thrown()
+        println "Here is the exception2 message: ${exception2.message}" 
+        exception2.message == "5 is a long outside the values 10 and 9223372036854775807"
+        println "lr.firstNum: ${lr.firstNum}, lr.secondNum: ${lr.secondNum}, lr.thirdNum: ${lr.thirdNum}"
+        lr.firstNum  == 50L
+        lr.secondNum == 50L
+        lr.thirdNum  == 50L
+        
+        when:
         lr.thirdNum  = 1001L
         lr.firstNum  = 1001L
         lr.secondNum = 1001L
-
         then:
-        thrown( GroovyValidatorException )
+        final GroovyValidatorException exception3 = thrown()
+        println "Here is the exception3 message: ${exception3.message}" 
+        exception3.message == "1001 is a long outside the values 0 and 1000"
         println "lr.firstNum: ${lr.firstNum}, lr.secondNum: ${lr.secondNum}, lr.thirdNum: ${lr.thirdNum}"
         lr.firstNum  == 50L
         lr.secondNum == 50L
