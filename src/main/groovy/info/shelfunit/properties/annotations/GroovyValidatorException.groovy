5b696dc484c53cf73a289185a016af51030fd82c
Eric MacAdie <Eric@MacAdie.net>
2014-08-17 21:37:11 -0500

    Moving some FloatRunners

--- src/main/groovy/info/shelfunit/properties/annotations/GroovyValidatorException.groovy
+++ src/main/groovy/info/shelfunit/properties/annotations/GroovyValidatorException.groovy
@@ -1,14 +1,18 @@
 package info.shelfunit.properties.annotations
 
 /**
-This is a class that will catch an exception in the validating classes.
+This is a class that will catch an exception in the validating classes. If it is enabled, it will give messages like
+<pre>
+5 is a long outside the values 10 and 9223372036854775807
+</pre>
 */
 
 class GroovyValidatorException extends Exception {
     GroovyValidatorException() {
         super()
     }
-    
+    /**
+    */
     GroovyValidatorException( String message ) {
         super( message )
     }
