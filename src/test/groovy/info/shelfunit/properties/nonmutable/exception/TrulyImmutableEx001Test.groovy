778f8262796c582facf15c4f1dc68bb0b4631c5e
Eric MacAdie <Eric@MacAdie.net>
2014-08-23 09:24:50 -0500

    Working on some stuff

--- src/test/groovy/info/shelfunit/properties/nonmutable/exception/TrulyImmutableEx001Test.groovy
+++ src/test/groovy/info/shelfunit/properties/nonmutable/exception/TrulyImmutableEx001Test.groovy
@@ -25,58 +25,83 @@ class TrulyImmutableEx001Test extends Specification {
         when:
         def throwaway = new TrulyImmutableEx001( [ firstString: "Not Junk", secondString: "Goodbye Junk", firstInt: 21, secondInt: 20 ], true, true )
         boolean exceptionThrown = false
+        /*
         try {
             throwaway.firstString = "Throwaway"
         } catch ( Exception e ) {
             exceptionThrown = true
         }
+        */
         then:
-        throwaway.firstString == "Not Junk"
-        throwaway.firstInt == 0
-        throwaway.secondInt == 0
-        exceptionThrown == true
+        def ex = thrown( Exception )
+        ex.message == "Groovy validation exception: \n" +
+        "21 is an integer outside the range 30 and 400 \n" +
+        "20 is an integer outside the range 30 and 400 "
+        // throwaway.firstString == "Not Junk"
+        // throwaway.firstInt == 0
+        // throwaway.secondInt == 0
+        // exceptionThrown == true
         println "Just made throwaway, about to make bTest1"
         
         when:
         def bTest1 = new TrulyImmutableEx001( [ firstString: "Hello1", secondString: "Goodbye", firstInt: 21, secondInt: 200 ], true, true )
         println "In test ${name.methodName}, bTest1: ${bTest1.toString()}"
         then:
-        bTest1.firstString == "Hello1"
-        bTest1.secondInt == 200
-        bTest1.firstInt == 0
+        def ex2 = thrown( Exception )
+        ex2.message == "Groovy validation exception: \n" +
+        "21 is an integer outside the range 30 and 400 "
+        // bTest1.firstString == "Hello1"
+        // bTest1.secondInt == 200
+        // bTest1.firstInt == 0
         
     } // end "test the no arg constructor"
     
     def "test bTest2"() {
         println "\n\n--- Starting test ${name.methodName}"
+        when:
         def bTest2 = new TrulyImmutableEx001( [ firstString: "Hello2", secondString: "Goodbye, this is more than 20 characters", firstInt: 22, secondInt: 20 ], true, true )
         println "In test ${name.methodName}, bTest2: ${bTest2.toString()}"
-        expect:
-        bTest2.firstString == "Hello2"
-        bTest2.secondString == null
-        bTest2.firstInt == 0
-        bTest2.secondInt == 0
+        then:
+        def ex = thrown( Exception )
+        ex.message == "Groovy validation exception: \n" +
+            "\"Goodbye, this is more than 20 characters\" is a String with a length outside the range of 5 to 20 characters \n" +
+            "22 is an integer outside the range 30 and 400 \n" +
+            "20 is an integer outside the range 30 and 400 "
+        // bTest2.firstString == "Hello2"
+        // bTest2.secondString == null
+        // bTest2.firstInt == 0
+        // bTest2.secondInt == 0
     } // end "test bTest2"
     
     def "third test"() {
         println "\n\n--- Starting test ${name.methodName}"
-
-        boolean exceptionThrown = false
+        when:
+        def testString = "hello, this is a test"
+        // boolean exceptionThrown = false
         def bTest1 = new TrulyImmutableEx001( [ firstString: "Hello3", secondString: "Goodbye", secondInt: 401, firstInt: 21 ], true, true )
         println "In test ${name.methodName}, bTest1: ${bTest1.toString()}"
         // println "bTest1.firstString: ${bTest1.firstString}, bTest1.secondString: ${bTest1.secondString}"
+        /*
         try {
             bTest1.secondString = "ChumbaWumba"
         } catch ( Exception e ) {
             exceptionThrown = true
         }
+        */
         println "Still in test ${name.methodName}, bTest1: ${bTest1.toString()}"
-        expect:
-        bTest1.firstString == "Hello3"
-        bTest1.secondString == "Goodbye"
-        bTest1.firstInt == 0
-        bTest1.secondInt == 0
-        exceptionThrown == true
+        then:
+        def exThirdTest = thrown( Exception )
+        exThirdTest.message == "Groovy validation exception: \n" +
+        "21 is an integer outside the range 30 and 400 \n" +
+        "401 is an integer outside the range 30 and 400 "
+        testString == "hello, this is a test"
+        testString.length() == 21
+        println "Here is testString.length(): ${testString.length()}"
+        // bTest1.firstString == "Hello3"
+        // bTest1.secondString == "Goodbye"
+        // bTest1.firstInt == 0
+        // bTest1.secondInt == 0
+        // exceptionThrown == true
     } // end "test the no arg constructor again"
    
 } // TrulyImmutableEx001Test 
