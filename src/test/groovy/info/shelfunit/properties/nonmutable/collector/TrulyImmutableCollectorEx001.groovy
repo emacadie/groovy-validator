b9787c704461c4b2bf76b253f040353f4e43ad2a
Eric MacAdie <Eric@MacAdie.net>
2014-09-03 02:07:33 -0500

    Working on tests with AnnotationCollector

---
+++ src/test/groovy/info/shelfunit/properties/nonmutable/collector/TrulyImmutableCollectorEx001.groovy
@@ -0,0 +1,21 @@
+package info.shelfunit.properties.nonmutable.collector
+
+import info.shelfunit.properties.annotations.ImmutableValidator
+import info.shelfunit.properties.annotations.IntAnnotation
+import info.shelfunit.properties.annotations.StringAnnotation
+
+@ImmutableValidator
+class TrulyImmutableCollectorEx001 {
+
+    @StringAnnotation( minLength = 5, maxLength = 200 )
+    String firstString
+    @StringAnnotation( minLength = 5, maxLength = 20 )
+    String secondString
+    @IntAnnotation( minValue = 30, maxValue = 400 )
+    int firstInt
+    @IntAnnotation( minValue = 30, maxValue = 400 )
+    int secondInt
+   
+} // TrulyImmutable001
+
+
---
+++ src/test/groovy/info/shelfunit/properties/nonmutable/collector/TrulyImmutableCollectorEx001Test.groovy
@@ -0,0 +1,82 @@
+package info.shelfunit.properties.nonmutable.collector
+
+import spock.lang.Specification
+import org.junit.Rule
+import org.junit.rules.TestName
+
+class TrulyImmutableCollectorEx001Test extends Specification {
+    def setup() {}       // run before every feature method
+    def cleanup() {}     // run after every feature method
+    def setupSpec() {
+    }   // run before the first feature method
+    def cleanupSpec() {} // run after the last feature method
+    
+    @Rule 
+    TestName name = new TestName()
+   
+    def "two immutable objects with validation, trying to change the first"() {
+        println "\n\n--- Starting test ${name.methodName}"
+        println "About to make throwaway"
+        when:
+        def throwaway = new TrulyImmutableCollector001( [ firstString: "Not Junk", secondString: "Goodbye Junk", firstInt: 21, secondInt: 20 ], true, true )
+        boolean exceptionThrown = false
+        try {
+            throwaway.firstString = "Throwaway"
+        } catch ( Exception e ) {
+            exceptionThrown = true
+        }
+        then:
+        def ex1 = thrown( Exception )
+        ex1.message == "Groovy validation exception: \n" +
+        "21 is an integer outside the range 30 and 400 \n" +
+        "20 is an integer outside the range 30 and 400 "
+        exceptionThrown == false
+        println "Just made throwaway, about to make bTest1"
+        
+        when:
+        def bTest1 = new TrulyImmutableCollector001( [ firstString: "Hello1", secondString: "Goodbye", firstInt: 21, secondInt: 200 ], true, true )
+        println "In test ${name.methodName}, bTest1: ${bTest1.toString()}"
+        then:
+        def ex2 = thrown ( Exception )
+        ex2.message == "Groovy validation exception: \n" +
+        "21 is an integer outside the range 30 and 400 "
+        
+    } // end "two immutable objects with validation, trying to change the first"
+    
+    def "test bTest2"() {
+        println "\n\n--- Starting test ${name.methodName}"
+        when:
+        def bTest2 = new TrulyImmutableCollector001( [ firstString: "Hello2", secondString: "Goodbye, this is more than 20 characters", firstInt: 22, secondInt: 20 ], true, true )
+        println "In test ${name.methodName}, bTest2: ${bTest2.toString()}"
+        then:
+        def exTest2 = thrown( Exception )
+        exTest2.message == "Groovy validation exception: \n" +
+        "\"Goodbye, this is more than 20 characters\" is a String with a length outside the range of 5 to 20 characters \n" +
+        "22 is an integer outside the range 30 and 400 \n" +
+        "20 is an integer outside the range 30 and 400 "
+    } // end "test bTest2"
+    
+    def "third test"() {
+        println "\n\n--- Starting test ${name.methodName}"
+        when:
+        boolean exceptionThrown = false
+        def bTest1 = new TrulyImmutableCollector001( [ firstString: "Hello3", secondString: "Goodbye", secondInt: 401, firstInt: 21 ], true, true )
+        println "In test ${name.methodName}, bTest1: ${bTest1.toString()}"
+        
+        try {
+            bTest1.secondString = "ChumbaWumba"
+        } catch ( Exception e ) {
+            exceptionThrown = true
+        }
+        println "Still in test ${name.methodName}, bTest1: ${bTest1.toString()}"
+        then:
+        def exThirdTest = thrown( Exception )
+        exThirdTest.message == "Groovy validation exception: \n" +
+        "21 is an integer outside the range 30 and 400 \n" +
+        "401 is an integer outside the range 30 and 400 "
+        exceptionThrown == false
+    } // end "third test
+   
+} // TrulyImmutableCollectorEx001Test 
+
+
