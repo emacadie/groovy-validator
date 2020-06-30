22be396775bde5099f503d19890452cb8c8c2a31
Eric MacAdie <eric@MacAdie.net>
2020-06-30 01:51:17 -0500

    Got tests to start working again

--- src/test/groovy/info/shelfunit/properties/sample/BookSequelSpec.groovy
+++ src/test/groovy/info/shelfunit/properties/sample/BookSequelSpec.groovy
@@ -9,7 +9,7 @@ class BookSequelSpec extends Specification {
     def setup() {}          // run before every feature method
     def cleanup() {}        // run after every feature method
     def setupSpec() { }     // run before the first feature method
-    def cleanupSpec() {}   // run after the last feature method
+    def cleanupSpec() {}    // run after the last feature method
     
     @Rule 
     TestName name = new TestName()
